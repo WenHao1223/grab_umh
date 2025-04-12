1bf6e4d7c45ca47b0fe8d959d693d4212329f701
WenHao1223 <wenhaojshs@gmail.com>
2025-04-12 09:41:48 +0800

    Setup google map

--- lib/src/pages/home/home_view.dart
+++ lib/src/pages/home/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:grab_umh/src/settings/settings_view.dart';
+import 'package:grab_umh/src/utils/constants/colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,17 +15,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GoogleMapController? _googleMapController;
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(5.285153, 100.456238),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('G Crab'), actions: [
-        IconButton(
-          icon: const Icon(Icons.settings),
-          onPressed: () {
-            Navigator.restorablePushNamed(context, SettingsView.routeName);
-          },
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          setState(() {
+            _googleMapController = controller;
+          });
+        },
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: GCrabColors.buttonPrimary,
+        foregroundColor: GCrabColors.white,
+        onPressed: () => _googleMapController
+          ?..animateCamera(CameraUpdate.newCameraPosition(_kGooglePlex)),
+        child: const Icon(
+          Icons.center_focus_strong,
         ),
-      ]),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _googleMapController?.dispose();
+    super.dispose();
   }
 }
